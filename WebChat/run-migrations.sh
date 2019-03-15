676e7dcb80872ef4d0515b4a9c0df0ee8fc52973
SuperDru <30354748+SuperDru@users.noreply.github.com>
2019-03-15 12:15:17 +0300

    <master> docker change directories

--- WebChat/run-migrations.sh
+++ WebChat/run-migrations.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+
 ./liquibase --changeLogFile=./Migrations/index.xml \
 --username=$username \
 --password=$password \
 --url=jdbc:postgresql://$host:$port/$dbname \
 --driver=org.postgresql.Driver \
 --classpath=./postgresql-42.2.5.jar \
---logLevel=$loglevel
+--logLevel=$loglevel \
 update
